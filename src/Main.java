85b49fb1c35f4d6691d6706761d2f72c80e0b629
Alexey <alexey_kuzminykh@mail.ru>
2022-10-15 11:21:34 +0300

    HW 2.2

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,45 @@ public class Main {
         int saving = 15000;
         int total = 0;
         int i = 0;
-        while (total < 2_459_000) {
-            total = total + total / 100;
+        for (; total < 12_000_000; i++) {
+            total = total + total * 7 / 100;
             total = total + saving;
-            i = i + 1;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей.");
         }
         // Задание 2
         System.out.println("Задание 2");
-        int number = 1;
-        while (number <= 10) {
-            System.out.print(number + " ");
-            number++;
+        int saving2 = 15000;
+        int total2 = 0;
+        int i2 = 0;
+        for (; total2 <= 12_000_000; i2++) {
+            total2 = total2 + total2 * 7 / 100;
+            total2 = total2 + saving2;
+            if (i2 % 6 == 0) {
+                System.out.println("Месяц " + i2 + ", сумма накоплений равна " + total2 + " рублей.");
+            }
         }
-        System.out.println();
-        for (int number2 = 10; number2 >= 1; number2--) {
-            System.out.print(number2 + " ");
-        }
-        System.out.println();
+
         // Задание 3
         System.out.println("Задание 3");
-        int population = 12_000_000;
-        int born = 17;
-        int death = 8;
-        int balance = born - death;
-        for(int year = 1; year <= 10; year++) {
-            population += population * balance / 1000;
-            System.out.println("Год " + year + ", численность населения составляет " + population);
+        int saving3 = 15000;
+        int total3 = 0;
+        for (int i3 = 1; i3 <= 108; i3++) {
+            total3 = total3 + total3 * 7 / 100;
+            total3 = total3 + saving3;
+            if (i3 % 6 == 0) {
+                System.out.println("Месяц " + i3 + ", сумма накоплений равна " + total3 + " рублей.");
+            }
+        }
+        // Задание 4
+        System.out.println("Задание 4");
+        int friday = 7;
+        for ( friday =1; friday <=31; friday ++){
+            if (friday % 7 ==0) {
+                System.out.println("Сегодня пятница,"+ friday + "-е число. Необходимо подготовить отчет.");
+            }
         }
     }
 }
+
+
 
